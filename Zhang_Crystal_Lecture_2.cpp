80b8d23a3aecec55c48f2cfefe6347360a444bfe
Crystal Zhang <crystalz16@nycstudents.net>
2024-09-17 15:58:20 -0400

    Update Zhang_Crystal_Lecture_2.cpp

--- Zhang_Crystal_Lecture_2.cpp
+++ Zhang_Crystal_Lecture_2.cpp
@@ -7,31 +7,39 @@
 using namespace std; 
 
 int main() {
-//Creating Variables
-    int Num = 5; 
-    float FloatNum = 5.99;
-    double DoubleNum = 9.98;
-    char Letter = 'C';
-    bool Boolean = true; 
-    string String = "Hello";
+    //Creating Variables
+    int Num = 5;                 // defining int value
+    float FloatNum = 6.88;       // defining float value 
+    double DoubleNum = 9.12;     // defining double value
+    char Letter = 'C';           // defining char value
+    bool Boolean = true;         // defining bool value
+    string String = "Hello";     // defining string value 
 
+    //defining car values to compare 
+    char Letter1 = 'j';          //defining letter1 value
+    char Letter2 = 'p';          //defining letter2 value
+
+    cout << "j < p = " << (Letter1 < Letter2) <<  endl; //comparing letter 1 and letter 2, if true, will show a 1
+
+    //code from slide 37 
     int inches; 
-    inches = 100;
+    inches = 100;                                 //inches to be converted
+    cout << inches << " inch(es) = ";             
+    cout << inches / 12 << " feet (foot) and ";   //converted inches to feet
+    cout << inches % 12 << " inch(es)" << endl;   //show remaining unconverted inches
 
-    cout << inches << " inche(es) =";
-    cout << inches / 12 << " feet (foot) and";
-    cout << inches % 12 << "inch(es)" << endl; 
 
-    cout << "int: " << Num << "\n"; 
-    cout << "float: " << FloatNum << "\n"; 
-    cout << "double: " << DoubleNum << "\n"; 
-    cout << "char: " << Letter << "\n";
-    cout << "bool: " << Boolean << "\n"; 
-    cout << "string: " << String << "\n"; 
+    cout << "Int: " << Num << "\n";              //show value to int
+    cout << "Float: " << FloatNum << "\n";       //show value to float
+    cout << "Double: " << DoubleNum << "\n";     //show value to double
+    cout << "Char: " << Letter << "\n";          //show value to char
+    cout << "Bool: " << Boolean << "\n";         //show value to bool
+    cout << "String: " << String << "\n";        //show value to string
 
-    cout << "2 + 3.5 = " << 2 + 3.5 << endl; 
+    //doing and showing math equations 
+    cout << "2 + 3.5 = " << 2 + 3.5 << endl;           
     cout << "6 / 4 + 3.9 = " << 6 / 4 + 3.9 << endl; 
     cout << "5.4 * 2 - (13.6 + 18 / 2) = " << 5.4 * 2 - (13.6 + 18 / 2) << endl;
 
-return 0; 
+    return 0; 
 }
